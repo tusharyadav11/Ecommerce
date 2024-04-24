27fcdfd57bf734c530713ebae7432766b1e6b91c
Tushar Yadav <145464824+tyadav47711@users.noreply.github.com>
2024-04-24 10:14:46 +0000

    login logout functionalities

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -32,14 +32,47 @@ export const cookieOptions= {
      res.cookie("token",token,cookieOptions)    // first token is the name of the cookie and second is the 
      // creation of the token ie above it only and third  is cookieoptions 
 
-     res.status().json{
+     res.status().json({
         success: true,
         token,
         user
-     }
-
-    
-    
-    }
-
+     }) 
  })
+
+export const login = asynchandler(async (req,res)=>{
+    const {email,password} = req.body
+
+    if(!email || !password){
+        return new customError("Both the field are required",400);
+    }
+  const user =   User.findOne({email}).select("+password")
+
+  if(!user){
+    return new customError("you entered the wrong credentials ",400)
+  }
+  const ismatched = await user.comparePassword(password)
+if(!ismatched){
+    throw new customError("wrong credentials ",400)
+}
+    const token = user.getJwtToken()
+    user.password= undefined
+    res.cookie("token",token,cookieOptions)
+    return res.send(200).json({
+        success: true,
+        user,
+        token
+    })
+
+})
+
+export const logout = asynchandler(async (req,res)=>{
+     res.cookie("token",null,{
+        expires: new Date(Date.now()),
+        httpOnly: true
+     })
+
+     res.status(200).json({
+        success: true,
+        message: "logged out "
+     })
+})
