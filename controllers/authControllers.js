659769a813fb061d83d5295399810fb022be6d16
Tushar Yadav <145464824+tyadav47711@users.noreply.github.com>
2024-04-24 09:31:47 +0000

    auth controller- signup

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,4 @@
+import user from "../models/user";
 import User from "../models/user";
 import asynchandler from '../services/asynchandler'
 import customError from '../utils/customError'
@@ -6,4 +7,39 @@ export const cookieOptions= {
     expires: new Date(Date.now()+ 3*24*60*60*1000),
     httpOnly: true
     
-}
+ }
+
+ export const signup = asynchandler(async (req,res)=>{
+    const {name,email,password} =req.body
+
+    if(!name || !email || !password){
+        throw new customError("All the field are required to be filled",400)
+      }
+      // check if user exists 
+     const existingUser=  await User.findOne({email})
+     if(existingUser){
+        throw new customError("User already exists",400)
+     }
+     const user = await User.create({
+        name,
+        email,
+        password
+     });
+     // creating up the token 
+     const token = user.getJwtToken()
+     user.password= undefined
+
+     res.cookie("token",token,cookieOptions)    // first token is the name of the cookie and second is the 
+     // creation of the token ie above it only and third  is cookieoptions 
+
+     res.status().json{
+        success: true,
+        token,
+        user
+     }
+
+    
+    
+    }
+
+ })
