4ae4aceec17a6697c17d41b26a452b4c864e9c0e
Tushar Yadav <145464824+tyadav47711@users.noreply.github.com>
2024-04-25 08:28:43 +0000

    creating and updating the collection

---
+++ controllers/collection.js
@@ -0,0 +1,64 @@
+ import collections from '../models/collections'
+ import asynchandler from '../services/asynchandler'
+ import customError from '../utils/customError'
+
+ // creating the collection 
+ export const createCollection = asynchandler(async (req,res)=>{
+    // take the name from the frontend 
+const  {name} = req.body
+if(!name){
+    throw new customError('name of the collection is mandatory ',400)  
+}
+
+// now taken the name and now we have to put it to the databsse
+   const collectionname=  await collections.create({
+        name 
+    })
+
+    // throwing it now to the frontend
+    res.status(200).json({
+        success: true,
+        message: "collection thrown with success",
+        collectionname
+    })
+
+ })
+
+ // now updating the value 
+ export const updateCollection = asynchandler(async(req,res)=>{
+        // existing value to be updated
+    const {id: collectionId} = req.params
+        // creating new value and adding 
+        const  {name} = req.body
+if(!name){
+    throw new customError('name of the collection is mandatory ',400)  
+}
+    let updatedCollectionname= await  Collection.findByIdAndUpdate(
+        collectionId,
+        {name},
+    
+        {new: true,
+        runValidators: true} 
+    )
+    if(!updatedCollectionname){
+        throw new customError('collection name is not updateda as collectionId does not found in database',400)
+    
+    }
+    // send response to the frontend 
+    res.status(200).json({
+        success: true,
+        message: 'collection updated successfully  ',
+        updateCollection
+    })
+ })
+
+ // deleting the collection 
+ export const deleteCollection = asynchandler(async(req,res)=>{
+
+    const {id: collectionId} = req.params
+
+    const collectionToDelete = await Collection.findByIdAndDelete(collectionId)
+ })
+
+
+
